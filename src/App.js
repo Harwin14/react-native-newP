3f381c469e5a99838277615dbe0a5c1e6f1b5672
Harwin14 <agungharwinn@gmail.com>
2023-05-14 19:54:22 +0700

    r

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import SampleComponent from './pages/SampleComponent';
 import StyleComponent from './pages/StyleComponent';
 import Layout from './pages/Layout';
 import Position from './pages/Position';
+import Flexbox from './pages/Flexbox';
 
 const App = () => {
   return (
     <View>
       <ScrollView>
-        <SampleComponent />
+        {/* <SampleComponent />
         <StyleComponent /> 
         <Layout /> 
-        <Position />
+        <Position /> */}
+        <Flexbox />
       </ScrollView>
     </View>
-  );
+  ); 
 };
 
 export default App;
---
+++ src/pages/Flexbox/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+class Flexbox extends Component {
+  constructor(props){
+    super(props)
+    console.log('==>>>constructor');
+  }
+  render() {
+    return (
+      <View style={{flexDirection:'row',borderColor:'#c8d6e5',justifyContent:'space-between'}}>
+        <View style={{backgroundColor:'#ee5253', flex:1,height:50}}/>
+        <View style={{backgroundColor:'#feca57', flex:1,height:50}}/>
+        <View style={{backgroundColor:'#1dd1a1', flex:1,height:50}}/>
+        <View style={{backgroundColor:'#5f27cd', flex:1,height:50}}/>
+      </View>
+    );
+  }
+} 
+
+export default Flexbox;
